b16c80060670b0c8f69f06dece37937d87cf9d77
Artemowkin <artyom.groshev2017@gmail.com>
2021-11-30 21:17:43 +0700

    Added products to shop page

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from . import views
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='home/')),
     path('home/', views.HomePageView.as_view(), name='homepage'),
     path(
         'shop/<uuid:pk>/', views.ConcreteProductView.as_view(),
