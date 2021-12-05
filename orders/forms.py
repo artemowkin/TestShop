39d1f5d2c4143eca019b6c3cfb8a44734072d8a8
Artemowkin <artyom.groshev2017@gmail.com>
2021-12-05 15:14:09 +0700

    Added page with creation order form

--- orders/forms.py
+++ orders/forms.py
@@ -4,14 +4,30 @@ from django import forms
 
 
 class CreateOrderForm(forms.Form):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'placeholder': 'Ivan'}
+    ))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'placeholder': 'Ivanov'}
+    ))
     phone = forms.RegexField(
         regex=r"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$",
-        error_messages={'invalid': 'Phone number is incorrect'}
+        error_messages={'invalid': 'Phone number is incorrect'},
+        widget=forms.TextInput(attrs={'placeholder': '8 XXX XXX-XX-XX'})
     )
-    city = forms.CharField(max_length=100)
-    street = forms.CharField(max_length=100)
-    house = forms.CharField(max_length=5)
-    apartment = forms.CharField(max_length=5)
-    postal_code = forms.CharField(max_length=6, min_length=6)
+    city = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'placeholder': 'Moscow'}
+    ))
+    street = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'placeholder': 'Pushkina'}
+    ))
+    house = forms.CharField(max_length=5, widget=forms.TextInput(
+        attrs={'placeholder': '25'}
+    ))
+    apartment = forms.CharField(max_length=5, widget=forms.TextInput(
+        attrs={'placeholder': '81'}
+    ))
+    postal_code = forms.CharField(
+        max_length=6, min_length=6,
+        widget=forms.TextInput(attrs={'placeholder': '198510'})
+    )
