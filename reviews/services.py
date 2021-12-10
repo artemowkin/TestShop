d142affa6e700c8890a60eed91a687a9f7ab8431
Artemowkin <artyom.groshev2017@gmail.com>
2021-12-10 11:27:43 +0700

    Fixed: not authenticated user now can't create a review for product

--- reviews/services.py
+++ reviews/services.py
@@ -1,6 +1,7 @@
 from django.db.models import QuerySet
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
+from django.core.exceptions import PermissionDenied
 
 from .models import Review
 from products.models import Product
@@ -29,6 +30,7 @@ class CreateReviewService:
         Create a new review for product from user (if authenticated)
         with rating and text
         """
+        if not user.is_authenticated: raise PermissionDenied
         product = self._get_products_service.get_concrete(product_pk)
         review = self._model(
             product=product, user=user, rating=rating, text=text
